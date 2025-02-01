07b7135c5dfdeef64734df00ebd568c91f6b468b
kavinraj7305 <kavinrajnandakumar@gmail.com>
2025-02-02 03:28:39 +0530

    task-progressed changed

--- app/dashboard/_components/task-progressed-card.tsx
+++ app/dashboard/_components/task-progressed-card.tsx
@@ -2,31 +2,27 @@
 
 import { Card } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
-import { api } from "@/convex/_generated/api"
-import { useAuth } from "@clerk/nextjs"
-import { useQuery } from "convex/react"
+import { Doc } from "@/convex/_generated/dataModel"
 
-const TaskProgressedCard = () => {
-    const { userId } = useAuth()
-    const tasks = useQuery(api.tasks.getTasks, {
-        userId: userId || ''
-    })
-    if (!tasks) {
-        return <div>Loading...</div>
-    }
-    const completedTasks = tasks.filter(task => task.isDone).length
-    const completionPercentage = (completedTasks / tasks.length) * 100
-    return(
-        <Card className="mb-6 shadow-lg rounded-2xl overflow-hidden">
+interface TaskProgressCardProps {
+  tasks: Doc<'tasks'>[]
+}
+
+const TaskProgressCard = ({ tasks }: TaskProgressCardProps) => {
+  const completedTasks = tasks.filter(task => task.isDone).length
+  const completionPercentage = (completedTasks / tasks.length) * 100 || 0 // Added fallback for 0
+
+  return (
+    <Card className="mb-6 shadow-lg rounded-2xl overflow-hidden">
       <div className="p-6">
-        <h2 className="text-xl font-semibold  mb-4">Task Progress</h2>
+        <h2 className="text-xl font-semibold mb-4">Task Progress</h2>
         <Progress value={completionPercentage} className="h-2 bg-gray-200" />
         <p className="text-sm mt-2">
           {completedTasks} of {tasks.length} tasks completed
         </p>
       </div>
     </Card>
-    )
+  )
 }
 
-export default TaskProgressedCard
+export default TaskProgressCard
